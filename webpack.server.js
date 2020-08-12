45a94d2664765352cb37f16a5b8cf1dd12000bda
Freddie Lumor <freddielumor@gmail.com>
2020-08-12 22:26:15 +0100

    Optimisation - Reduce server bundle size via webpackNodeExternals

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const merge = require("webpack-merge");
 const baseConfig = require("./webpack.base.js");
+const webpackNodeExternals = require("webpack-node-externals");
 
 const config = {
   // Tell webpack we're building a bundle for Node, not browser
@@ -12,6 +13,8 @@ const config = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "build"),
   },
+
+  externals: [webpackNodeExternals()], // Do not add any packages in node_modules into final SS bundle
 };
 
 module.exports = merge(baseConfig, config);
