f47c6205ef6aaa01f42a4256041da2251d527fc4
Freddie Lumor <freddielumor@gmail.com>
2020-08-13 17:27:48 +0100

    Redux
    - Setup getUser action & reducer

---
+++ client/src/actions/index.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+import { GET_USERS_ENDPOINT } from "../helpers/apiConfig";
+
+export const FETCH_USERS = "FETCH_USERS";
+
+export const fetchUsers = () => async (dispatch) => {
+  const res = await axios.get(GET_USERS_ENDPOINT);
+  dispatch({
+    type: FETCH_USERS,
+    payload: res,
+  });
+};
---
+++ client/src/helpers/apiConfig.js
@@ -0,0 +1,1 @@
+export const GET_USERS_ENDPOINT = "http://react-ssr-api.herokuapp.com/users";
