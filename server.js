1640a15ab0bb353fe08583148a20e2f21bbe7c2e
Freddie Lumor <freddielumor@gmail.com>
2020-08-13 17:05:10 +0100

    Redux setup
    - Setup initial server & client redux store's.

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import express from "express";
 import renderer from "./client/src/helpers/renderer";
+import createStore from "./client/src/helpers/createStore";
 
 const app = express();
 
 app.use(express.static("public"));
 
 app.get("*", (req, res) => {
+  const store = createStore();
+
   // Call renderer function to send HTML to browser
-  res.send(renderer(req));
+  res.send(renderer(req, store));
 });
 
 app.listen(3000, () => {
